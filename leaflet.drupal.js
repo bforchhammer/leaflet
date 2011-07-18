e9139a321430ae945f561a6bec7b23e52d31efc2
Lev Tsypin <lev@levelos.com>
2011-07-18 16:48:10 -0700

    Initial commit.

---
+++ leaflet.drupal.js
@@ -0,0 +1,16 @@
+Drupal.behaviors.leaflet = {
+  attach: function (context, settings) {
+    jQuery(settings.leaflet).each(function() {
+      console.log(this);
+      var map = new L.Map(this.mapId, {
+          center: new L.LatLng(this.features[0]['lat'], this.features[0]['lon']), 
+          zoom: 5//this.zoom
+      });
+      
+      var tileUrl = this.layers[0],
+          layer = new L.TileLayer(tileUrl, {maxZoom: 18, scheme: 'tms'});
+      
+      map.addLayer(layer);          
+    });
+  }
+};
---
+++ leaflet_map.tpl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @file
+ * Template for a leaflet map
+ */
+?>
+<div id="<?php print $map_id ?>" style="height: 400px"></div>
