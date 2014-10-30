7881940af48e600cd4537bbe65d85bb17e42addf
Benedikt Forchhammer <b.forchhammer@gmail.com>
2014-10-30 13:45:33 +0100

    Fixed leaflet map view.

--- leaflet_views/src/Plugin/views/style/LeafletMap.php
+++ leaflet_views/src/Plugin/views/style/LeafletMap.php
@@ -7,7 +7,11 @@
 
 namespace Drupal\leaflet_views\Plugin\views\style;
 
-use Drupal\Component\Annotation\Plugin;
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\views\Annotation\ViewsStyle;
 use Drupal\Core\Annotation\Translation;
 use Drupal\views\ViewExecutable;
 use Drupal\views\Plugin\views\display\DisplayPluginBase;
@@ -21,7 +25,7 @@ use Drupal\views\Plugin\views\style\StylePluginBase;
  *
  * Attributes set below end up in the $this->definition[] array.
  *
- * @Plugin(
+ * @ViewsStyle(
  *   id = "leafet_map",
  *   title = @Translation("Leaflet map"),
  *   help = @Translation("Displays a View as a Leaflet map."),
@@ -50,8 +54,8 @@ class LeafletMap extends StylePluginBase {
     // For later use, set entity info related to the View's base table.
     $base_tables = array_keys($view->getBaseTables());
     $base_table = reset($base_tables);
-    foreach (entity_get_info() as $key => $info) {
-      if (isset($info['base_table']) && $info['base_table'] == $base_table) {
+    foreach (\Drupal::entityManager()->getDefinitions() as $key => $info) {
+      if ($info->getBaseTable() == $base_table) {
         $this->entity_type = $key;
         $this->entity_info = $info;
         return;
@@ -77,24 +81,29 @@ class LeafletMap extends StylePluginBase {
   /**
    * Options form
    */
-  public function buildOptionsForm(&$form, &$form_state) {
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
     parent::buildOptionsForm($form, $form_state);
 
     // Get a list of fields and a sublist of geo data fields in this view
     $fields = array();
     $fields_geo_data = array();
     foreach ($this->displayHandler->getHandlers('field') as $field_id => $handler) {
-      $label = $handler->label() ?: $field_id;
+      $label = $handler->adminLabel() ?: $field_id;
       $fields[$field_id] = $label;
-      if (!empty($handler->field_info['type']) && $handler->field_info['type'] == 'geofield') {
-        $fields_geo_data[$field_id] = $label;
+      if (is_a($handler, '\Drupal\field\Plugin\views\field\Field')) {
+        $field_storage_definitions = \Drupal::entityManager()->getFieldStorageDefinitions($handler->getEntityType());
+        $field_storage_definition = $field_storage_definitions[$handler->definition['field_name']];
+
+        if ($field_storage_definition->getType() == 'geofield') {
+          $fields_geo_data[$field_id] = $label;
+        }
       }
     }
 
     // Check whether we have a geo data field we can work with
     if (!count($fields_geo_data)) {
       $form['error'] = array(
-        '#markup' => t('Please add at least one geofield to the view.'),
+        '#markup' => $this->t('Please add at least one geofield to the view.'),
       );
       return;
     }
@@ -102,8 +111,8 @@ class LeafletMap extends StylePluginBase {
     // Map preset.
     $form['data_source'] = array(
       '#type' => 'select',
-      '#title' => t('Data Source'),
-      '#description' => t('Which field contains geodata?'),
+      '#title' => $this->t('Data Source'),
+      '#description' => $this->t('Which field contains geodata?'),
       '#options' => $fields_geo_data,
       '#default_value' => $this->options['data_source'],
       '#required' => TRUE,
@@ -112,8 +121,8 @@ class LeafletMap extends StylePluginBase {
     // Name field
     $form['name_field'] = array(
       '#type' => 'select',
-      '#title' => t('Title Field'),
-      '#description' => t('Choose the field which will appear as a title on tooltips.'),
+      '#title' => $this->t('Title Field'),
+      '#description' => $this->t('Choose the field which will appear as a title on tooltips.'),
       '#options' => array_merge(array('' => ''), $fields),
       '#default_value' => $this->options['name_field'],
     );
@@ -122,14 +131,14 @@ class LeafletMap extends StylePluginBase {
     // Add an option to render the entire entity using a view mode
     if ($this->entity_type) {
       $desc_options += array(
-        '#rendered_entity' => '<' . t('!entity entity', array('!entity' => $this->entity_type)) . '>',
+        '#rendered_entity' => '<' . $this->t('!entity entity', array('!entity' => $this->entity_type)) . '>',
       );
     }
 
     $form['description_field'] = array(
       '#type' => 'select',
-      '#title' => t('Description Field'),
-      '#description' => t('Choose the field or rendering method which will appear as a description on tooltips or popups.'),
+      '#title' => $this->t('Description Field'),
+      '#description' => $this->t('Choose the field or rendering method which will appear as a description on tooltips or popups.'),
       '#required' => FALSE,
       '#options' => $desc_options,
       '#default_value' => $this->options['description_field'],
@@ -139,15 +148,15 @@ class LeafletMap extends StylePluginBase {
 
       // Get the human readable labels for the entity view modes.
       $view_mode_options = array();
-      foreach (entity_get_view_modes($this->entity_type) as $key => $view_mode) {
+      foreach (\Drupal::entityManager()->getViewModes($this->entity_type) as $key => $view_mode) {
         $view_mode_options[$key] = $view_mode['label'];
       }
-      // The View Mode drop-down is visibile conditional on "#rendered_entity"
+      // The View Mode drop-down is visible conditional on "#rendered_entity"
       // being selected in the Description drop-down above.
       $form['view_mode'] = array(
         '#type' => 'select',
-        '#title' => t('View mode'),
-        '#description' => t('View modes are ways of displaying entities.'),
+        '#title' => $this->t('View mode'),
+        '#description' => $this->t('View modes are ways of displaying entities.'),
         '#options' => $view_mode_options,
         '#default_value' => !empty($this->options['view_mode']) ? $this->options['view_mode'] : 'full',
         '#states' => array(
@@ -162,10 +171,10 @@ class LeafletMap extends StylePluginBase {
     // Choose a map preset
     $map_options = array();
     foreach (leaflet_map_get_info() as $key => $map) {
-      $map_options[$key] = t($map['label']);
+      $map_options[$key] = $this->t($map['label']);
     }
     $form['map'] = array(
-      '#title' => t('Map'),
+      '#title' => $this->t('Map'),
       '#type' => 'select',
       '#options' => $map_options,
       '#default_value' => $this->options['map'] ?: '',
@@ -173,45 +182,45 @@ class LeafletMap extends StylePluginBase {
     );
 
     $form['height'] = array(
-      '#title' => t('Map height'),
+      '#title' => $this->t('Map height'),
       '#type' => 'textfield',
-      '#field_suffix' => t('px'),
+      '#field_suffix' => $this->t('px'),
       '#size' => 4,
       '#default_value' => $this->options['height'],
       '#required' => FALSE,
     );
 
     $form['icon'] = array(
-      '#title' => t('Map Icon'),
+      '#title' => $this->t('Map Icon'),
       '#type' => 'fieldset',
       '#collapsible' => TRUE,
       '#collapsed' => !isset($this->options['icon']['iconUrl']),
     );
 
     $form['icon']['iconUrl'] = array(
-      '#title' => t('Icon URL'),
-      '#description' => t('Can be an absolute or relative URL.'),
+      '#title' => $this->t('Icon URL'),
+      '#description' => $this->t('Can be an absolute or relative URL.'),
       '#type' => 'textfield',
       '#maxlength' => 999,
       '#default_value' => $this->options['icon']['iconUrl'] ?: '',
     );
 
     $form['icon']['shadowUrl'] = array(
-      '#title' => t('Icon Shadow URL'),
+      '#title' => $this->t('Icon Shadow URL'),
       '#type' => 'textfield',
       '#maxlength' => 999,
       '#default_value' => $this->options['icon']['shadowUrl'] ?: '',
     );
 
     $form['icon']['iconSize'] = array(
-      '#title' => t('Icon Size'),
+      '#title' => $this->t('Icon Size'),
       '#type' => 'fieldset',
       '#collapsible' => FALSE,
-      '#description' => t('Size of the icon image in pixels.')
+      '#description' => $this->t('Size of the icon image in pixels.')
     );
 
     $form['icon']['iconSize']['x'] = array(
-      '#title' => t('Width'),
+      '#title' => $this->t('Width'),
       '#type' => 'textfield',
       '#maxlength' => 3,
       '#size' => 3,
@@ -220,7 +229,7 @@ class LeafletMap extends StylePluginBase {
     );
 
     $form['icon']['iconSize']['y'] = array(
-      '#title' => t('Height'),
+      '#title' => $this->t('Height'),
       '#type' => 'textfield',
       '#maxlength' => 3,
       '#size' => 3,
@@ -229,14 +238,14 @@ class LeafletMap extends StylePluginBase {
     );
 
     $form['icon']['iconAnchor'] = array(
-      '#title' => t('Icon Anchor'),
+      '#title' => $this->t('Icon Anchor'),
       '#type' => 'fieldset',
       '#collapsible' => FALSE,
-      '#description' => t('The coordinates of the "tip" of the icon (relative to its top left corner). The icon will be aligned so that this point is at the marker\'s geographical location.')
+      '#description' => $this->t('The coordinates of the "tip" of the icon (relative to its top left corner). The icon will be aligned so that this point is at the marker\'s geographical location.')
     );
 
     $form['icon']['iconAnchor']['x'] = array(
-      '#title' => t('X'),
+      '#title' => $this->t('X'),
       '#type' => 'textfield',
       '#maxlength' => 3,
       '#size' => 3,
@@ -245,7 +254,7 @@ class LeafletMap extends StylePluginBase {
     );
 
     $form['icon']['iconAnchor']['y'] = array(
-      '#title' => t('Y'),
+      '#title' => $this->t('Y'),
       '#type' => 'textfield',
       '#maxlength' => 3,
       '#size' => 3,
@@ -254,13 +263,13 @@ class LeafletMap extends StylePluginBase {
     );
 
     $form['icon']['shadowAnchor'] = array(
-      '#title' => t('Shadow Anchor'),
+      '#title' => $this->t('Shadow Anchor'),
       '#type' => 'fieldset',
       '#collapsible' => FALSE,
-      '#description' => t('The point from which the shadow is shown.')
+      '#description' => $this->t('The point from which the shadow is shown.')
     );
     $form['icon']['shadowAnchor']['x'] = array(
-      '#title' => t('X'),
+      '#title' => $this->t('X'),
       '#type' => 'textfield',
       '#maxlength' => 3,
       '#size' => 3,
@@ -268,7 +277,7 @@ class LeafletMap extends StylePluginBase {
       '#element_validate' => array('form_validate_number'),
     );
     $form['icon']['shadowAnchor']['y'] = array(
-      '#title' => t('Y'),
+      '#title' => $this->t('Y'),
       '#type' => 'textfield',
       '#maxlength' => 3,
       '#size' => 3,
@@ -277,14 +286,14 @@ class LeafletMap extends StylePluginBase {
     );
 
     $form['icon']['popupAnchor'] = array(
-      '#title' => t('Popup Anchor'),
+      '#title' => $this->t('Popup Anchor'),
       '#type' => 'fieldset',
       '#collapsible' => FALSE,
-      '#description' => t('The point from which the marker popup opens, relative to the anchor point.')
+      '#description' => $this->t('The point from which the marker popup opens, relative to the anchor point.')
     );
 
     $form['icon']['popupAnchor']['x'] = array(
-      '#title' => t('X'),
+      '#title' => $this->t('X'),
       '#type' => 'textfield',
       '#maxlength' => 3,
       '#size' => 3,
@@ -293,7 +302,7 @@ class LeafletMap extends StylePluginBase {
     );
 
     $form['icon']['popupAnchor']['y'] = array(
-      '#title' => t('Y'),
+      '#title' => $this->t('Y'),
       '#type' => 'textfield',
       '#maxlength' => 3,
       '#size' => 3,
@@ -305,25 +314,27 @@ class LeafletMap extends StylePluginBase {
   /**
    * Validates the options form.
    */
-  public function validateOptionsForm(&$form, &$form_state) {
+  public function validateOptionsForm(&$form, FormStateInterface $form_state) {
     parent::validateOptionsForm($form, $form_state);
 
-    $style_options = $form_state['values']['style_options'];
+    // @todo Validation fails if fields are left empty; should probably just
+    //       pass and use defaults.
+    $style_options = $form_state->getValue('style_options');
     if (!is_numeric($style_options['height']) || $style_options['height'] <= 0) {
-      form_error($form['height'], t('Map height needs to be a positive number.'));
+      $form_state->setError($form['height'], $this->t('Map height needs to be a positive number.'));
     }
     $icon_options = $style_options['icon'];
-    if (!empty($icon_options['iconUrl']) && !valid_url($icon_options['iconUrl'])) {
-      form_error($form['icon']['iconUrl'], t('Icon URL is invalid.'));
+    if (!empty($icon_options['iconUrl']) && !UrlHelper::isValid($icon_options['iconUrl'])) {
+      $form_state->setError($form['icon']['iconUrl'], $this->t('Icon URL is invalid.'));
     }
-    if (!empty($icon_options['shadowUrl']) && !valid_url($icon_options['shadowUrl'])) {
-      form_error($form['icon']['shadowUrl'], t('Shadow URL is invalid.'));
+    if (!empty($icon_options['shadowUrl']) && !UrlHelper::isValid($icon_options['shadowUrl'])) {
+      $form_state->setError($form['icon']['shadowUrl'], $this->t('Shadow URL is invalid.'));
     }
     if (!is_numeric($icon_options['iconSize']['x']) || $icon_options['iconSize']['x'] <= 0) {
-      form_error($form['icon']['iconSize']['x'], t('Icon width needs to be a positive number.'));
+      $form_state->setError($form['icon']['iconSize']['x'], $this->t('Icon width needs to be a positive number.'));
     }
     if (!is_numeric($icon_options['iconSize']['y']) || $icon_options['iconSize']['y'] <= 0) {
-      form_error($form['icon']['iconSize']['x'], t('Icon height needs to be a positive number.'));
+      $form_state->setError($form['icon']['iconSize']['x'], $this->t('Icon height needs to be a positive number.'));
     }
   }
 
@@ -349,7 +360,7 @@ class LeafletMap extends StylePluginBase {
 
           // Render the entity with the selected view mode
           if ($this->options['description_field'] === '#rendered_entity' && is_object($result)) {
-            $entity = entity_load($this->entity_type, $result->{$this->entity_info['entity_keys']['id']});
+            $entity = entity_load($this->entity_type, $result->{$this->entity_info->getKey('id')});
             $build = entity_view($entity, $this->options['view_mode']);
             $description = drupal_render($build);
           }
@@ -382,9 +393,8 @@ class LeafletMap extends StylePluginBase {
         }
       }
 
-      $map = leaflet_map_get_info($this->options['map']);
-
       if (!empty($data)) {
+        $map = leaflet_map_get_info($this->options['map']);
         return leaflet_render_map($map, $data, $this->options['height'] . 'px');
       }
     }
